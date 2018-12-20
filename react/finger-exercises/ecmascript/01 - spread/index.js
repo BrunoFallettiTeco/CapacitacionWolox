9226bcdee22867532cc26dee1871bde841fd3429
Bruno Falletti <BFalletti@proveedor.teco.com.ar>
2018-12-20 12:31:18 -0300

    Added solution for the third test - min can return the minimum based on many parameters

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -1,10 +1,10 @@
 import { isArray } from './utils';
 
-export function min(nums) {
-  if (!isArray(nums)) {
-    return nums;
+export function min(...nums) {
+  if (!isArray(nums[0])) {
+    return Math.min(...nums);
   }
-  return Math.min(...nums);
+  return Math.min(...nums[0]);
 }
 
 export function copy() {
